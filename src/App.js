2c8a941502b97f808c867de2f2f4bf7da09ae4e8
unknown <rahulkhatri72925@gmail.com>
2021-01-24 15:48:36 +0530

    https to https change

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends React.Component {
   setWeather = () => {
     const city = this.state.searchBarInput;
     const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-    const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
     this.setState(
       {
